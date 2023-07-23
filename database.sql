48d581f9c58b972173c5c221bb4465e0daf5a1c1
Giulio Bello <giuliobello12@gmail.com>
2023-07-23 15:42:03 +0200

    check primo carettere id sensore

--- database.sql
+++ database.sql
@@ -6,21 +6,21 @@ CREATE DATABASE iot;
 USE iot;
 
 CREATE TABLE IF NOT EXISTS oxygen_sensor (
-    id VARCHAR(5) NOT NULL,
+    id VARCHAR(5) NOT NULL CHECK (id LIKE 'o%'),
     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
     value DECIMAL(5, 2) CHECK (value >= 40 AND value <= 100),
     PRIMARY KEY (id, timestamp)
 );
 
 CREATE TABLE IF NOT EXISTS heartbeat_sensor (
-    id VARCHAR(5) NOT NULL,
+    id VARCHAR(5) NOT NULL CHECK (id LIKE 'h%'),
     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
     value INTEGER CHECK (value >= 50 AND value <= 140),
     PRIMARY KEY (id, timestamp)
 );
 
 CREATE TABLE IF NOT EXISTS temperature_sensor (
-    id VARCHAR(5) NOT NULL,
+    id VARCHAR(5) NOT NULL CHECK (id LIKE 't%'),
     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
     value DECIMAL(5, 2) CHECK (value >= 34 AND value <= 45),
     PRIMARY KEY (id, timestamp)
