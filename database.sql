a2ee0a430ecbc6f6069a93368099443204b8417c
Giulio Bello <giuliobello12@gmail.com>
2023-08-03 19:40:28 +0200

    aggiustamenti DB

--- database.sql
+++ database.sql
@@ -8,21 +8,21 @@ USE iot;
 CREATE TABLE IF NOT EXISTS oxygen_sensor (
     id VARCHAR(5) NOT NULL,
     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-    value INTEGER,
+    value INTEGER NOT NULL,
     PRIMARY KEY (id, timestamp)
 );
 
 CREATE TABLE IF NOT EXISTS cardio_sensor (
     id VARCHAR(5) NOT NULL,
     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-    value INTEGER,
+    value INTEGER NOT NULL,
     PRIMARY KEY (id, timestamp)
 );
 
 CREATE TABLE IF NOT EXISTS troponin_sensor (
     id VARCHAR(5) NOT NULL,
     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-    value DECIMAL(5, 2),
+    value DECIMAL(5, 2) NOT NULL,
     PRIMARY KEY (id, timestamp)
 );
 
@@ -41,8 +41,7 @@ BEGIN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'ID must start with "o"';
     END IF;
-END;
-//
+END;//
 DELIMITER ;
 
 DELIMITER //
@@ -54,8 +53,7 @@ BEGIN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'ID must start with "c"';
     END IF;
-END;
-//
+END;//
 DELIMITER ;
 
 DELIMITER //
@@ -67,45 +65,44 @@ BEGIN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'ID must start with "t"';
     END IF;
-END;
-//
+END;//
 DELIMITER ;
 
 -- Inserisci 10 valori nella tabella oxygen_sensor
 INSERT INTO oxygen_sensor (id, timestamp, value) VALUES 
-('o1', NOW(), 40 + RAND() * 60),
-('o1', DATE_ADD(NOW(), INTERVAL 1 SECOND), 40 + RAND() * 60),
-('o1', DATE_ADD(NOW(), INTERVAL 2 SECOND), 40 + RAND() * 60),
-('o1', DATE_ADD(NOW(), INTERVAL 3 SECOND), 40 + RAND() * 60),
-('o1', DATE_ADD(NOW(), INTERVAL 4 SECOND), 40 + RAND() * 60),
-('o1', DATE_ADD(NOW(), INTERVAL 5 SECOND), 40 + RAND() * 60),
-('o1', DATE_ADD(NOW(), INTERVAL 6 SECOND), 40 + RAND() * 60),
-('o1', DATE_ADD(NOW(), INTERVAL 7 SECOND), 40 + RAND() * 60),
-('o1', DATE_ADD(NOW(), INTERVAL 8 SECOND), 40 + RAND() * 60),
-('o1', DATE_ADD(NOW(), INTERVAL 9 SECOND), 40 + RAND() * 60);
+('o1', NOW(), FLOOR(40 + RAND() * 60)),
+('o1', DATE_ADD(NOW(), INTERVAL 1 SECOND), FLOOR(40 + RAND() * 60)),
+('o1', DATE_ADD(NOW(), INTERVAL 2 SECOND), FLOOR(40 + RAND() * 60)),
+('o1', DATE_ADD(NOW(), INTERVAL 3 SECOND), FLOOR(40 + RAND() * 60)),
+('o1', DATE_ADD(NOW(), INTERVAL 4 SECOND), FLOOR(40 + RAND() * 60)),
+('o1', DATE_ADD(NOW(), INTERVAL 5 SECOND), FLOOR(40 + RAND() * 60)),
+('o1', DATE_ADD(NOW(), INTERVAL 6 SECOND), FLOOR(40 + RAND() * 60)),
+('o1', DATE_ADD(NOW(), INTERVAL 7 SECOND), FLOOR(40 + RAND() * 60)),
+('o1', DATE_ADD(NOW(), INTERVAL 8 SECOND), FLOOR(40 + RAND() * 60)),
+('o1', DATE_ADD(NOW(), INTERVAL 9 SECOND), FLOOR(40 + RAND() * 60));
 
--- Inserisci 10 valori nella tabella heartbeat_sensor
-INSERT INTO heartbeat_sensor (id, timestamp, value) VALUES 
-('h1', NOW(), FLOOR(50 + RAND() * 90)),
-('h1', DATE_ADD(NOW(), INTERVAL 1 SECOND), FLOOR(50 + RAND() * 90)),
-('h1', DATE_ADD(NOW(), INTERVAL 2 SECOND), FLOOR(50 + RAND() * 90)),
-('h1', DATE_ADD(NOW(), INTERVAL 3 SECOND), FLOOR(50 + RAND() * 90)),
-('h1', DATE_ADD(NOW(), INTERVAL 4 SECOND), FLOOR(50 + RAND() * 90)),
-('h1', DATE_ADD(NOW(), INTERVAL 5 SECOND), FLOOR(50 + RAND() * 90)),
-('h1', DATE_ADD(NOW(), INTERVAL 6 SECOND), FLOOR(50 + RAND() * 90)),
-('h1', DATE_ADD(NOW(), INTERVAL 7 SECOND), FLOOR(50 + RAND() * 90)),
-('h1', DATE_ADD(NOW(), INTERVAL 8 SECOND), FLOOR(50 + RAND() * 90)),
-('h1', DATE_ADD(NOW(), INTERVAL 9 SECOND), FLOOR(50 + RAND() * 90));
+-- Inserisci 10 valori nella tabella cardio_sensor
+INSERT INTO cardio_sensor (id, timestamp, value) VALUES 
+('c1', NOW(), FLOOR(50 + RAND() * 90)),
+('c1', DATE_ADD(NOW(), INTERVAL 1 SECOND), FLOOR(50 + RAND() * 90)),
+('c1', DATE_ADD(NOW(), INTERVAL 2 SECOND), FLOOR(50 + RAND() * 90)),
+('c1', DATE_ADD(NOW(), INTERVAL 3 SECOND), FLOOR(50 + RAND() * 90)),
+('c1', DATE_ADD(NOW(), INTERVAL 4 SECOND), FLOOR(50 + RAND() * 90)),
+('c1', DATE_ADD(NOW(), INTERVAL 5 SECOND), FLOOR(50 + RAND() * 90)),
+('c1', DATE_ADD(NOW(), INTERVAL 6 SECOND), FLOOR(50 + RAND() * 90)),
+('c1', DATE_ADD(NOW(), INTERVAL 7 SECOND), FLOOR(50 + RAND() * 90)),
+('c1', DATE_ADD(NOW(), INTERVAL 8 SECOND), FLOOR(50 + RAND() * 90)),
+('c1', DATE_ADD(NOW(), INTERVAL 9 SECOND), FLOOR(50 + RAND() * 90));
 
--- Inserisci 10 valori nella tabella temperature_sensor
-INSERT INTO temperature_sensor (id, timestamp, value) VALUES 
-('t1', NOW(), 34 + RAND() * 11),
-('t1', DATE_ADD(NOW(), INTERVAL 1 SECOND), 34 + RAND() * 11),
-('t1', DATE_ADD(NOW(), INTERVAL 2 SECOND), 34 + RAND() * 11),
-('t1', DATE_ADD(NOW(), INTERVAL 3 SECOND), 34 + RAND() * 11),
-('t1', DATE_ADD(NOW(), INTERVAL 4 SECOND), 34 + RAND() * 11),
-('t1', DATE_ADD(NOW(), INTERVAL 5 SECOND), 34 + RAND() * 11),
-('t1', DATE_ADD(NOW(), INTERVAL 6 SECOND), 34 + RAND() * 11),
-('t1', DATE_ADD(NOW(), INTERVAL 7 SECOND), 34 + RAND() * 11),
-('t1', DATE_ADD(NOW(), INTERVAL 8 SECOND), 34 + RAND() * 11),
-('t1', DATE_ADD(NOW(), INTERVAL 9 SECOND), 34 + RAND() * 11);
+-- Inserisci 10 valori nella tabella troponin_sensor
+INSERT INTO troponin_sensor (id, timestamp, value) VALUES 
+('t1', NOW(), 0.01 + RAND() * 1),
+('t1', DATE_ADD(NOW(), INTERVAL 1 SECOND), 0.01 + RAND() * 1),
+('t1', DATE_ADD(NOW(), INTERVAL 2 SECOND), 0.01 + RAND() * 1),
+('t1', DATE_ADD(NOW(), INTERVAL 3 SECOND), 0.01 + RAND() * 1),
+('t1', DATE_ADD(NOW(), INTERVAL 4 SECOND), 0.01 + RAND() * 1),
+('t1', DATE_ADD(NOW(), INTERVAL 5 SECOND), 0.01 + RAND() * 1),
+('t1', DATE_ADD(NOW(), INTERVAL 6 SECOND), 0.01 + RAND() * 1),
+('t1', DATE_ADD(NOW(), INTERVAL 7 SECOND), 0.01 + RAND() * 1),
+('t1', DATE_ADD(NOW(), INTERVAL 8 SECOND), 0.01 + RAND() * 1),
+('t1', DATE_ADD(NOW(), INTERVAL 9 SECOND), 0.01 + RAND() * 1);
